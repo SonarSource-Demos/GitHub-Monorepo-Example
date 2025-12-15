d3cc7cc5fd35a472c6e965704ea1284459e294bd
alexandravaron <alex.varon@sonarsource.com>
2025-12-15 13:44:54 -0600

    Update MyClass.cs

--- comp-dotnet/project/MyClass.cs
+++ comp-dotnet/project/MyClass.cs
@@ -9,11 +9,6 @@ namespace HelloWorld
             int await = 42;
             int async = 42;
             return "Happy coding!";
-filter = "{" + request.Key + ":'" + emailAddress + "'}";
-List<Customer> Donor = Providers.Donor.GetDonorByIdOrEmailAddress(filter, memberId);
-
-string endpointCustomer = string.Format("v1.3/search?q={0}", filter);
-var restContentCustomer = StoreAccess.GetRESTResponse(endpointCustomer, token);
         }
     }
 }
